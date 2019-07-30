e84f36ca6d305f34c3308d8f398963431049f22e
Ernesto Serrano <info@ernesto.es>
2019-07-30 16:30:50 +0200

    Code cleanup

--- startup.js
+++ startup.js
@@ -1,7 +1,7 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 module.exports = async () => {
-  browser.params.password = 'P2qfBG5UPYh5FhwM';
+  browser.params.password = (process.env.MASTER_PASSWORD || 'P2qfBG5UPYh5FhwM');
   // browser.params.password = '1234';
   // Add jasmine spec reporter
   jasmine.getEnv().addReporter(new SpecReporter({
