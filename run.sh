7ac8999fc41ee12784e8a492bfb01bbc09cc5775
Ernesto Serrano <info@ernesto.es>
2019-09-19 15:00:36 +0200

    Fix wait for backend in run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,12 @@ while ! nc -z backend 8000; do
   echo "Site $DOMAIN_NAME unavailable..."
   sleep 1s
 done
+# TODO: Use healthcheck instead of backend for avoid mistakes
+# # Wait to webserver to be available
+# until $(curl --output /dev/null --silent --head --fail ${DOMAIN_NAME}/healthcheck/); do
+#   echo "Site $DOMAIN_NAME unavailable..."
+#   sleep 1s
+# done
 
 echo "Runing protractor..."
 protractor conf.js --suite regression --verbose
