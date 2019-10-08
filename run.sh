d981cae57d5c5db76e2a76b2b95ce58c7c155edc
Ernesto Serrano <info@ernesto.es>
2019-10-08 13:42:11 +0200

    Fix service

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ echo "Starting webdriver..."
 webdriver-manager start --detach
 
 # Wait to webserver to be available
-while ! nc -z backend 8000; do
+while ! nc -z service-exo-core 8000; do
   echo "Site $DOMAIN_NAME unavailable..."
   sleep 1s
 done
-# TODO: Use healthcheck instead of backend for avoid mistakes
+# TODO: Use healthcheck instead of service-exo-core for avoid mistakes
 # # Wait to webserver to be available
 # until $(curl --output /dev/null --silent --head --fail ${DOMAIN_NAME}/healthcheck/); do
 #   echo "Site $DOMAIN_NAME unavailable..."
